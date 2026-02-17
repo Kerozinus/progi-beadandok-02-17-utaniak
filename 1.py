9e7c5fe3f89d6684be9523e4ae3f0a01cea29f20
Kerozinus <ignath.benjamin.2024@osztalyterem.katolikuskeri.hu>
2026-02-17 16:18:29 +0100

    0.1.
    Első feladat kész

---
+++ 1.py
@@ -0,0 +1,33 @@
+lista = []
+eredmeny = 0
+def run():
+    with open ("pontok.txt","r",encoding="utf-8") as f:
+        for i in f:
+            lista.append(int(i))
+    eredmeny = lista[0]
+    for szam in range(1,len(lista)):
+        eredmeny = eredmeny + lista[szam]
+    print("A számok összege: %0.2f" % eredmeny)
+    eredmeny = lista[0]
+    for szam in range(1,len(lista)):
+        eredmeny = eredmeny + lista[szam]
+    eredmeny = eredmeny / len(lista)
+    print("A számok átlaga: %0.2f" % eredmeny)
+    eredmeny = max(lista)
+    print("A legnagyobb szám a listában: %0.2f" % eredmeny)
+
+temp = input("Meg győződtél róla hogy a listában 1 sorban 1 szám van? (y/n)")
+from time import sleep
+if temp == "y":
+    print("A program elindul.")
+    sleep(1)
+    print("Számok összegének kiszámítása...")
+    sleep(0.3)
+    print("Számok átlagának kiszámítása...")
+    sleep(0.3)
+    print("A legnagyobb szám megkeresése...")
+    sleep(0.3)
+    print("")
+    run()
+else:
+    print("Ez esetben a program működése megáll...")
